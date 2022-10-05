3a24f24dd1bb83e3243a65a7f4bf8a09bc642bc7
JK1331 <jakubkubala3@gmail.com>
2022-10-05 13:18:48 +0200

    Code refactoring

--- test/TokenSale.test.ts
+++ test/TokenSale.test.ts
@@ -1,16 +1,38 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { MyERC20, TokenSale } from "../typechain-types";
+
+const ERC20_TOKEN_RATIO = 5;
 
 describe("NFT Shop", () => {
-  beforeEach(() => {});
-
-  describe("When the Shop contract is deployed", () => {
-    it("defines the ratio as provided in parameters", () => {
-      throw new Error("Not implemented");
+    let tokenSaleContract: TokenSale;
+    let erc20Token: MyERC20;
+    let deployer: SignerWithAddress;
+    let acc1: SignerWithAddress;
+    let acc2: SignerWithAddress;
+    
+    beforeEach(async () => {
+        [deployer, acc1, acc2] = await ethers.getSigners();
+        const erc20TokenFactory = await ethers.getContractFactory("MyERC20")
+        const erc20Token = await erc20TokenFactory.deploy();
+        await erc20Token.deployed();
+        const tokenSaleContractFactory = await ethers.getContractFactory("TokenSale");
+        tokenSaleContract = await tokenSaleContractFactory.deploy(ERC20_TOKEN_RATIO, erc20Token.address);
+        await tokenSaleContract.deployed();
+        const MINTER_ROLE = await erc20Token.MINTER_ROLE();
+        const grantRoleTx = await erc20Token.grantRole(MINTER_ROLE, tokenSaleContract.address);
+        await grantRoleTx.wait();
     });
 
-    it("uses a valid ERC20 as payment token", () => {
-      throw new Error("Not implemented");
+    describe("When the Shop contract is deployed", () => {
+        it("defines the ratio as provided in parameters", async () => {
+            const rate = await tokenSaleContract.erc20Purchaseratio();
+            expect(rate).to.eq(ERC20_TOKEN_RATIO)
+        });
+
+        it("uses a valid ERC20 as payment token", () => {
+            throw new Error("Not implemented");
     });
   });
 
