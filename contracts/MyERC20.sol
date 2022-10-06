997ad92d969cb57355ea82bd3c57ec24af674ce1
JK1331 <jakubkubala3@gmail.com>
2022-10-06 18:35:51 +0200

    Refactoring token contract

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -2,18 +2,18 @@
 pragma solidity 0.8.8;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyERC20 is ERC20, AccessControl {
+contract MyERC20 is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("MyERC20Token", "MERC20") {
+    constructor() ERC20("MyToken", "MTK") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function mint(address _to, uint256 _amount) public onlyRole(MINTER_ROLE) {
-        _mint(_to, _amount);
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
     }
 }
